3a1c09fc1d93fa0946312522d9f2223cb9f220a8
DecentM <decentm@decentm.com>
2024-06-16 17:36:21 +0300

    fix: move dotfiles into /usr/share

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ cd "/tmp/build-${RELEASE}"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-cp -a /tmp/dotfiles /var/opt/
+cp -a /tmp/dotfiles /usr/share/dotfiles
 
 # add gcloud-cli repo
 tee -a /etc/yum.repos.d/google-cloud-sdk.repo <<EOM
