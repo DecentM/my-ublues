a88fc3983edccaa7deba6cdab979e90b3669823e
DecentM <decentm@decentm.com>
2024-06-16 18:27:19 +0300

    chore: remove autoupdate since we're now shipping dotfiles with the system

--- dotfiles/lib/completion.zsh
+++ dotfiles/lib/completion.zsh
@@ -6,7 +6,7 @@ autoload -Uz compinit && compinit
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 
-if command -v fzf >/dev/null && verlt '0.48.0' $(fzf --version); then
+if vercmd fzf && verlt '0.48.0' "$(fzf --version)"; then
     zstyle ':completion:*' menu no
     zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
 
---
+++ dotfiles/lib/utils.zsh
@@ -0,0 +1,30 @@
+#!/bin/false
+# shellcheck shell=sh
+
+verlte() {
+    [ "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]
+}
+
+verlt() {
+    [ "$1" = "$2" ] && return 1 || verlte $1 $2
+}
+
+vercmd() {
+    # Set +e to avoid crashing the script if the command is not found
+    local e_was_set=false
+
+    if [[ $- == *e* ]]; then
+        e_was_set=true
+        set +e
+    fi
+
+    command -v "$1" >/dev/null
+    local status=$?
+
+    # Restore the errexit option
+    if [ "$e_was_set" = true ]; then
+        set -e
+    fi
+
+    return $status
+}
