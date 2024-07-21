0f605a3bf9cafa455969d290220087951f4df79f
DecentM <decentm@decentm.com>
2024-07-21 13:21:48 +0300

    fix: include emulation and io cores in vm reservation

--- rootfs/etc/libvirt/hooks/lib/cpu/reserve-cores.sh
+++ rootfs/etc/libvirt/hooks/lib/cpu/reserve-cores.sh
@@ -5,7 +5,7 @@
 # Ex: 1st Core -> reserved=0
 # Ex: 1st & 2nt Cores -> reserved=0,1
 # Ex: 1st Physical Core (16 Virtual Cores) -> reserved=0,8
-reserved=14,15
+reserved=0,8,14,15
 
 systemctl set-property --runtime -- system.slice AllowedCPUs=$reserved
 systemctl set-property --runtime -- user.slice AllowedCPUs=$reserved
