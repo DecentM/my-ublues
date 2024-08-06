7964a46c8afa1f1ed9873272e06b6908d2aac119
DecentM <decentm@decentm.com>
2024-08-06 04:36:54 +0300

    chore: disable gcloud plugin, slight zplug reorganisation

--- files/system/usr/share/dotfiles/lib/zplug.zsh
+++ files/system/usr/share/dotfiles/lib/zplug.zsh
@@ -9,22 +9,27 @@ else
 
   zplug romkatv/powerlevel10k, as:theme, depth:1
   zplug hlissner/zsh-autopair, defer:2
-  zplug zsh-users/zsh-autosuggestions
-  zplug zsh-users/zsh-completions
   # zplug djui/alias-tips
   zplug zimfw/asdf
-  zplug wintermi/zsh-gcloud
+  # zplug wintermi/zsh-gcloud
+
+  # Fuzzy stuff
   zplug supercrabtree/k
   zplug jgogstad/zsh-mask
+  zplug Aloxaf/fzf-tab
 
+  # SSH
   zplug hkupty/ssh-agent
   zstyle :omz:plugins:ssh-agent agent-forwarding on
   zstyle :omz:plugins:ssh-agent identities id_rsa id_rsa2 id_github id_ed25519
   zstyle :omz:plugins:ssh-agent lifetime 8h
 
-  zplug Aloxaf/fzf-tab
-  # zplug joshskidmore/zsh-fzf-history-search
+  # Convenience
+  zplug zsh-users/zsh-autosuggestions
+  zplug zsh-users/zsh-completions
   zplug zsh-users/zsh-syntax-highlighting
+
+  # Language support
   zplug cmuench/zsh-miniconda
   zplug se-jaeger/zsh-activate-py-environment
 
