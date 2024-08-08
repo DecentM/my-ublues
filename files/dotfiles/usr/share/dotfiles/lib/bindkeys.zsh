ffea7b0d783e2145a573d63910775c9ed964b1a9
DecentM <decentm@decentm.com>
2024-08-08 23:29:36 +0300

    feat: move to chezmoi for dotfiles instead of shipping them with the OS

--- files/dotfiles/usr/share/dotfiles/lib/bindkeys.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/false
-# shellcheck shell=sh
-
-bindkey "^[[1;5C" forward-word
-bindkey "^[[1;5D" backward-word
-bindkey "^[[H" beginning-of-line
-bindkey "^[[F" end-of-line
-bindkey "^[[3~" delete-char-or-list
--- files/dotfiles/usr/share/dotfiles/zprofile
+++
@@ -1,8 +0,0 @@
-#!/bin/false
-# shellcheck shell=sh
-
-eval "$(ssh-agent -s)" >/dev/null
-ssh-add ~/.ssh/id_* >/dev/null
-export SSH_AGENT_SOCK=$SSH_AUTH_SOCK
-
-DOTFILES_BASEDIR=$(dirname $(realpath "$0"))
