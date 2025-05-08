e56d0c22f64a65f404fb20955ac850ead40b705d
DecentM <decentm@decentm.com>
2025-05-09 01:59:40 +0300

    feat(firewall): implement ability to add/remove zones and services

--- modules/firewall/firewall.sh
+++ modules/firewall/firewall.sh
@@ -34,18 +34,17 @@ EOF
     fi
 }
 
-# Add ports
-echo "$1" | jq -c '.ports // [] | .[]' | while read -r port; do
-    ZONE=$(echo "$port" | jq -r '.zone')
-    PORT=$(echo "$port" | jq -r '.port')
-    PROTO=$(echo "$port" | jq -r '.protocol')
-    ensure_zone_file "$ZONE"
-    ZONE_FILE="$ZONES_DIR/$ZONE.xml"
+# Remove existing services
+echo "$1" | jq -c '.remove_services // [] | .[]' | while read -r service; do
+    NAME=$(echo "$service" | jq -r '.name')
+    SERVICE_FILE="$SERVICES_DIR/$NAME.xml"
 
-    # Add port only if not present
-    if ! grep -q "port=\"$PORT\" protocol=\"$PROTO\"" "$ZONE_FILE"; then
-        echo "Adding port $PORT/$PROTO to zone $ZONE"
-        sed -i "/<\/zone>/i \  <port port=\"$PORT\" protocol=\"$PROTO\"/>" "$ZONE_FILE"
+    if [ -f "$SERVICE_FILE" ]; then
+        echo "Removing custom service: $NAME"
+        rm -f "$SERVICE_FILE"
+    else
+        echo "Service $NAME not found"
+        exit 1
     fi
 done
 
@@ -66,24 +65,107 @@ echo "$1" | jq -c '.custom_services // [] | .[]' | while read -r service; do
     } >"$SERVICE_FILE"
 done
 
-# Enable/disable services
-echo "$1" | jq -c '.services // [] | .[]' | while read -r svc; do
-    ZONE=$(echo "$svc" | jq -r '.zone')
-    NAME=$(echo "$svc" | jq -r '.name')
-    ENABLED=$(echo "$svc" | jq -r '.enabled')
-    ensure_zone_file "$ZONE"
-    ZONE_FILE="$ZONES_DIR/$ZONE.xml"
+# Remove existing zones
+echo "$1" | jq -c '.remove_zones // [] | .[]' | while read -r zone; do
+    NAME=$(echo "$zone" | jq -r '.name')
+    ZONE_FILE="$ZONES_DIR/$NAME.xml"
 
-    if [ "$ENABLED" = "true" ]; then
-        if ! grep -q "<service name=\"$NAME\"" "$ZONE_FILE"; then
-            echo "Enabling service $NAME in zone $ZONE"
-            sed -i "/<\/zone>/i \  <service name=\"$NAME\"/>" "$ZONE_FILE"
-        fi
+    if [ -f "$ZONE_FILE" ]; then
+        echo "Removing custom zone: $NAME"
+        rm -f "$ZONE_FILE"
     else
-        echo "Disabling service $NAME in zone $ZONE"
-        # Remove matching service entry
-        sed -i "/<service name=\"$NAME\"\/>/d" "$ZONE_FILE"
+        echo "Zone $NAME not found"
+        exit 1
     fi
+done
+
+# Create custom zones
+echo "$1" | jq -c '.custom_zones // [] | .[]' | while read -r zone; do
+    NAME=$(echo "$zone" | jq -r '.name')
+    DESC=$(echo "$zone" | jq -r '.description')
+    TARGET=$(echo "$zone" | jq -r '.target')
+    ZONE_FILE="$ZONES_DIR/$NAME.xml"
+    ensure_zone_file "$NAME"
+
+    echo "Writing custom zone: $NAME"
+    {
+        echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+        echo "<zone>"
+        echo "  <short>$NAME</short>"
+        echo "  <description>$DESC</description>"
+        echo "  <target>$TARGET</target>"
+        echo "$zone" | jq -r '.services // [] | .[] | "  <service name=\"\(.name)\"/>"'
+        echo "$zone" | jq -r '.ports // [] | .[] | "  <port protocol=\"\(.protocol)\" port=\"\(.port)\"/>"'
+        echo "$zone" | jq -r '.interfaces // [] | .[] | "  <interface name=\"\(.name)\"/>"'
+        echo "</zone>"
+    } >"$ZONE_FILE"
+done
+
+# # Bind services to zones
+# echo "$1" | jq -c '.services // [] | .[]' | while read -r svc; do
+#     ZONE=$(echo "$svc" | jq -r '.zone')
+#     NAME=$(echo "$svc" | jq -r '.name')
+#     ENABLED=$(echo "$svc" | jq -r '.enabled')
+#     ensure_zone_file "$ZONE"
+#     ZONE_FILE="$ZONES_DIR/$ZONE.xml"
+
+#     if [ "$ENABLED" = "true" ]; then
+#         if ! grep -q "<service name=\"$NAME\"" "$ZONE_FILE"; then
+#             echo "Enabling service $NAME in zone $ZONE"
+#             sed -i "/<\/zone>/i \  <service name=\"$NAME\"/>" "$ZONE_FILE"
+#         fi
+#     else
+#         echo "Disabling service $NAME in zone $ZONE"
+#         # Remove matching service entry
+#         sed -i "/<service name=\"$NAME\"\/>/d" "$ZONE_FILE"
+#     fi
+# done
+
+# # Bind ports to zones
+# echo "$1" | jq -c '.ports // [] | .[]' | while read -r port; do
+#     ZONE=$(echo "$port" | jq -r '.zone')
+#     PORT=$(echo "$port" | jq -r '.port')
+#     PROTO=$(echo "$port" | jq -r '.protocol')
+#     ENABLED=$(echo "$port" | jq -r '.enabled')
+#     ensure_zone_file "$ZONE"
+#     ZONE_FILE="$ZONES_DIR/$ZONE.xml"
+
+#     if [ "$ENABLED" = "true" ]; then
+#         if ! grep -q "port=\"$PORT\" protocol=\"$PROTO\"" "$ZONE_FILE"; then
+#             echo "Enabling port $PORT/$PROTO in zone $ZONE"
+#             sed -i "/<\/zone>/i \  <port port=\"$PORT\" protocol=\"$PROTO\"/>" "$ZONE_FILE"
+#         fi
+#     else
+#         echo "Disabling port $PORT/$PROTO in zone $ZONE"
+#         # Remove matching port entry
+#         sed -i "/<port port=\"$PORT\" protocol=\"$PROTO\"\/>/d" "$ZONE_FILE"
+#     fi
+# done
+
+# # Bind interfaces to zones
+# echo "$1" | jq -c '.interfaces // [] | .[]' | while read -r iface; do
+#     ZONE=$(echo "$iface" | jq -r '.zone')
+#     INTERFACE=$(echo "$iface" | jq -r '.interface')
+#     ENABLED=$(echo "$iface" | jq -r '.enabled')
+#     ensure_zone_file "$ZONE"
+#     ZONE_FILE="$ZONES_DIR/$ZONE.xml"
+
+#     if [ "$ENABLED" = "true" ]; then
+#         if ! grep -q "<interface name=\"$INTERFACE\"" "$ZONE_FILE"; then
+#             echo "Enabling interface $INTERFACE in zone $ZONE"
+#             sed -i "/<\/zone>/i \  <interface name=\"$INTERFACE\"/>" "$ZONE_FILE"
+#         fi
+#     else
+#         echo "Disabling interface $INTERFACE in zone $ZONE"
+#         # Remove matching interface entry
+#         sed -i "/<interface name=\"$INTERFACE\"\/>/d" "$ZONE_FILE"
+#     fi
+# done
+
+# Set default zone
+echo "$1" | jq -r '.default_zone' | while read -r zone; do
+    echo "Setting default zone to $zone"
+    sed -i "s|^DefaultZone=.*|DefaultZone=$zone|" /etc/firewalld/firewalld.conf
 done
 
 ostree container commit
